19659b61537bde93316ca0aae7e91cf073ca60e4
revindodewantara1 <63893659+revindodewantara1@users.noreply.github.com>
2020-04-18 11:58:03 +0700

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,18 @@
+<form action="proses-login.php" method="post">
+    <table>
+        <tr>
+            <td><label for="">Username</label></td>
+            <td> <input type="text" name="username" id=""></td>
+        </tr>
+        <tr>
+            <td><label for="">Password</label></td>
+            <td> <input type="password" name="password" id=""></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type="submit" value="Login" name="btnlogin">
+            </td>
+        </tr>
+    </table>
+</form>
